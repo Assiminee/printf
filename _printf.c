d1e114762f2eba2ac14bd918822402057c3ab39a
Benlem <zoubidabenlem@gmail.com>
2023-04-23 15:28:25 -0700

    removed condition

--- _printf.c
+++ _printf.c
@@ -49,13 +49,6 @@ int _printf(const char *format, ...)
 						length += ops[j].f(args);
 						break;
 					}
-					if(format[i+1] == *(ops[j].c))
-					{
-						length += ops[j].f(args);
-						length++;
-						i++;
-						break;
-					}
 					if (j == 3)
 					{
 						write(1, "%", 1);
