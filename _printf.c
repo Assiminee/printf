e50f30b6e7caafa696e17a14d36c046d7b2348bf
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-23 17:18:27 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -1,115 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
-int _strlen(char *s);
-int _printc(va_list l);
-int _prints(va_list l);
 /**
- * _printf - recreates the printf function
- * @format: string with format specifier
- * Return: number of characters printed
- */
-int _printf(const char *format, ...)
-{
-	int i = 0;
-	int j;
-	int length = 0;
-	operations ops[] = {
-		{"c", _printc},
-		{"s", _prints},
-		{NULL, NULL}
-	};
-	va_list args;
-
-	if (format == NULL)
-		return (-1);
-	if (format[0] == '%' && format[1] == '\0')
-		return (-1);
-	va_start(args, format);
-	while (format && format[i])
-	{
-		j = 0;
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '%')
-			{
-				write(1, &format[i], 1);
-				length++;
-			}
-			else
-			{
-				while (ops[j].c)
-				{
-					if (format[i] == *(ops[j].c))
-					{
-						length += ops[j].f(args);
-						break;
-					}
-					if (j == 1)
-					{
-						write(1, "%", 1);
-						write(1, &format[i], 1);
-						length += 2;
-						break;
-					}
-					j++;
-				}
-			}
-			i++;
-		}
-		else
-		{
-			write(1, &format[i], 1);
-			length++;
-			i++;
-		}
-	}
-	va_end(args);
-	return (length);
-}
-/**
- * _strlen:gets length of string
- * @s: string to check
- * Return: length of string
- */
+*_strlen:gets length of string
+*@s: string to check
+*Return: length of string
+*/
 int _strlen(char *s)
 {
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-	return (len);
-}
-
-/**
- * _printc - prints a single char
- * @l: A list of variadic arguments
- * Return: 1
- */
-int _printc(va_list l)
-{
-	int c = va_arg(l, int);
-
-	write(1, &c, 1);
-	return (1);
+    int len = 0;
+    while (*s != '\0')
+    {
+        len++;
+        s++;
+    }
+    return len;
 }
 /**
- * _prints - prints a string
- * @l: A list of variadic arguments
- * Return: length of string
- */
-int _prints(va_list l)
+*_printf:prints an input
+*@format: argument to print
+*Return: length of format
+*/
+int _printf(const char *format, ...)
 {
-	char *s = va_arg(l, char*);
-	int len = _strlen(s);
+    int i;
+    int c;
+    char *s;
+    int length;
+    va_list args;
 
-	if (s == NULL)
-	{
-		write(1, "(nil)", 5);
-		return (5);
-	}
-	write(1, s, len);
-	return (len);
+    i=0;
+    length=0;
+    va_start(args,format);
+    if(format==NULL)
+	    return(-1);
+    if (format[0] == '%' && format[1] == '\0')
+	return (-1);
+    while(format && format[i])
+    {
+        if(format[i]== '%')
+        {
+            i++;
+            if(format[i]=='s')
+            {
+                s = va_arg(args, char*);
+                if (s == NULL)
+                {
+                    write(1, "(nil)", 5);
+                    length += 5;
+                }
+                else
+                {
+                    length+=_strlen(s);
+                    write(1, s, _strlen(s));
+                }
+
+            }
+            else if(format[i]=='c')
+            {
+                length++;
+                c=va_arg(args,int);
+                write(1,&c,1);
+
+            }
+            else if(format[i]=='%')
+            {
+                length++;
+                write(1,&format[i],1);
+
+            }
+            else
+            {
+                length+=2;
+                write(1, "%", 1);
+                write(1, &format[i], 1);
+                break;
+            }
+            i++;
+        }
+        else
+        {
+            length++;
+            write(1, &format[i], 1);
+            i++;
+        }
+
+    }
+    va_end(args);
+    return(length);
 }
---
+++ tests/_printf.cu
@@ -0,0 +1,115 @@
+#include "main.h"
+int _strlen(char *s);
+int _printc(va_list l);
+int _prints(va_list l);
+/**
+ * _printf - recreates the printf function
+ * @format: string with format specifier
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	int j;
+	int length = 0;
+	operations ops[] = {
+		{"c", _printc},
+		{"s", _prints},
+		{NULL, NULL}
+	};
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+	if (format[0] == '%' && format[1] == '\0')
+		return (-1);
+	va_start(args, format);
+	while (format && format[i])
+	{
+		j = 0;
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '%')
+			{
+				write(1, &format[i], 1);
+				length++;
+			}
+			else
+			{
+				while (ops[j].c)
+				{
+					if (format[i] == *(ops[j].c))
+					{
+						length += ops[j].f(args);
+						break;
+					}
+					if (j == 1)
+					{
+						write(1, "%", 1);
+						write(1, &format[i], 1);
+						length += 2;
+						break;
+					}
+					j++;
+				}
+			}
+			i++;
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			length++;
+			i++;
+		}
+	}
+	va_end(args);
+	return (length);
+}
+/**
+ * _strlen:gets length of string
+ * @s: string to check
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * _printc - prints a single char
+ * @l: A list of variadic arguments
+ * Return: 1
+ */
+int _printc(va_list l)
+{
+	int c = va_arg(l, int);
+
+	write(1, &c, 1);
+	return (1);
+}
+/**
+ * _prints - prints a string
+ * @l: A list of variadic arguments
+ * Return: length of string
+ */
+int _prints(va_list l)
+{
+	char *s = va_arg(l, char*);
+	int len = _strlen(s);
+
+	if (s == NULL)
+	{
+		write(1, "(nil)", 5);
+		return (5);
+	}
+	write(1, s, len);
+	return (len);
+}
