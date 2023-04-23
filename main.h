86920cde47cdb40215082204a4c13feb79c8a2e6
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-23 17:03:15 +0000

    printf

--- main.h
+++ main.h
@@ -1,4 +1,14 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
 int _printf(const char *format, ...);
+typedef struct operations
+{
+    char *c;
+    int (*f)(va_list l);
+}operations;
 #endif
