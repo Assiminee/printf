77bccd91a974324481a54b6d113ee098f865924b
Benlem <zoubidabenlem@gmail.com>
2023-04-23 09:14:22 -0700

    eror

--- _printf.c
+++ _printf.c
@@ -11,13 +11,13 @@
 */
 int _strlen(char *s)
 {
-    int len = 0;
-    while (*s != '\0')
-    {
-        len++;
-        s++;
-    }
-    return len;
+	int len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+return (len);
 }
 /**
 *_printf:prints an input
@@ -26,69 +26,68 @@ int _strlen(char *s)
 */
 int _printf(const char *format, ...)
 {
-    int i;
-    int c;
-    char *s;
-    int length;
-    va_list args;
+	int i;
+	int c;
+	char *s;
+	int length;
+	va_list args;
 
-    i=0;
-    length=0;
-    va_start(args,format);
-    if(format==NULL)
-	    return(-1);
-    if (format[0] == '%' && format[1] == '\0')
-	return (-1);
-    while(format && format[i])
-    {
-        if(format[i]== '%')
-        {
-            i++;
-            if(format[i]=='s')
-            {
-                s = va_arg(args, char*);
-                if (s == NULL)
-                {
-                    write(1, "(nil)", 5);
-                    length += 5;
-                }
-                else
-                {
-                    length+=_strlen(s);
-                    write(1, s, _strlen(s));
-                }
+	i=0;
+	length=0;
 
-            }
-            else if(format[i]=='c')
-            {
-                length++;
-                c=va_arg(args,int);
-                write(1,&c,1);
+	va_start(args,format);
+	if(format == NULL)
+		return(-1);
+	if (format[0] == '%' && format[1] == '\0')
+		return (-1);
+	while(format && format[i])
+	{
+		if(format[i] == '%')
+		{
+			i++;
+			if(format[i] == 's')
+			{
+				s = va_arg(args, char*);
+				if (s == NULL)
+				{
+					write(1, "(nil)", 5);
+					length += 5;
+				}
+				else
+				{
+					length+=_strlen(s);
+					write(1, s, _strlen(s));
+				}
+			}	
+			else if(format[i] == 'c')
+			{
+				length++;
+				c=va_arg(args, int);
+				write(1, &c, 1);
+			}
+			else if(format[i] == '%')
+			{
+				length++;
+				write(1, &format[i], 1);
 
-            }
-            else if(format[i]=='%')
-            {
-                length++;
-                write(1,&format[i],1);
+			}
+			else
+			{
+				length+=2;
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				break;
+			}
+			i++;
+		}
+		else
+		{
+			length++;
+			write(1, &format[i], 1);
+			i++;
+		}
 
-            }
-            else
-            {
-                length+=2;
-                write(1, "%", 1);
-                write(1, &format[i], 1);
-                break;
-            }
-            i++;
-        }
-        else
-        {
-            length++;
-            write(1, &format[i], 1);
-            i++;
-        }
-
-    }
-    va_end(args);
-    return(length);
+	}
+	va_end (args);
+	return (length);
 }
