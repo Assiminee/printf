7eca942a9f9e60b0ee9f9ae1f28d8f36ea564690
Benlem <zoubidabenlem@gmail.com>
2023-04-24 08:39:17 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -7,15 +7,7 @@
 int _printf(const char *format, ...)
 {
 	int i = 0;
-	int j;
 	int length = 0;
-	operations ops[] = {
-		{"c", _printc},
-		{"s", _prints},
-		{"d", _printint},
-		{"i", _printint},
-		{NULL, NULL}
-	};
 	va_list args;
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
@@ -25,7 +17,6 @@ int _printf(const char *format, ...)
 		i++;
 	while (format && format[i])
 	{
-		j = 0;
 		if (format[i] == '%')
 		{
 			i++;
@@ -35,20 +26,7 @@ int _printf(const char *format, ...)
 			}
 			else
 			{
-				while (ops[j].c)
-				{
-					if (format[i] == *(ops[j].c))
-					{
-						length += ops[j].f(args);
-						break;
-					}
-					if (j == 3)
-					{
-						length += _invalid_spec(format[i]);
-						break;
-					}
-					j++;
-				}
+				length += handle_spec(format, i, length, args);
 			}
 			i++;
 		}
