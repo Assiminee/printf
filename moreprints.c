f57ff9a7e4d30304bd84bfd5d28dbfc61c5dd7eb
Benlem <zoubidabenlem@gmail.com>
2023-04-24 16:48:27 -0700

    printf

--- moreprints.c
+++ moreprints.c
@@ -8,4 +8,10 @@ int _printp()
 	write(1, "%", 1);
 	return (1);
 }
-
+/**
+ *_printsixteen - prints in base 16
+ *@n: number to print
+ *@r: digit to be orint next
+ *@tab: digits for conversion
+ @base : represents the base in which we print
+ */
