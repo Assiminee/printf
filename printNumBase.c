8949a3b8cbe559b08702a91d296b1c695596b1bf
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-25 14:23:39 +0000

    printf

--- printNumBase.c
+++ printNumBase.c
@@ -3,6 +3,7 @@
 /**
  * _printbin - prints a number in binary
  * @l: A list of variadic arguments
+ *
  * Return: length
  */
 int _printbin(va_list l)
@@ -11,9 +12,11 @@ int _printbin(va_list l)
 
 	return (_print_in_base(num, 2, " "));
 }
+
 /**
  * _printx - prints a number in hexadecimal in lowercase
  * @l: A list of variadic arguments
+ *
  * Return: number of characters printed
  */
 int _printx(va_list l)
@@ -22,9 +25,11 @@ int _printx(va_list l)
 
 	return (_print_in_base(num, 16, "0123456789abcdef"));
 }
+
 /**
  * _printX — prints a number in hexadecimal in uppercase
  * @l: A list of variadic arguments
+ *
  * Return: number of characters printed
  */
 int _printX(va_list l)
@@ -33,9 +38,11 @@ int _printX(va_list l)
 
 	return (_print_in_base(num, 16, "0123456789ABCDEF"));
 }
+
 /**
  * _printO - prints a number in base 8
  * @l: A list of variadix arguments
+ *
  * Return: number of characters printed
  */
 int _printO(va_list l)
