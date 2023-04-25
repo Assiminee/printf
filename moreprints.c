266b76acac8855d5aed8f4d74f1bbe29b24e8bd6
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-04-25 15:57:48 +0000

    printf

--- moreprints.c
+++ moreprints.c
@@ -20,7 +20,7 @@ int nonPrintableChar(va_list l)
 		s = "(null)";
 	while (s && s[i])
 	{
-		if (s[i] <= 32 || s[i] >= 127)
+		if (s[i] <= 32 && s[i] >= 127)
 		{
 			write(1, "\\x", 2);
 			_print_in_base(s[i], 16, "0123456789ABCDEF");
