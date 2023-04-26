868d80330eea71cef30e93cbafc2bbf7257adc94
Benlem <zoubidabenlem@gmail.com>
2023-04-25 17:22:34 -0700

    printf

--- functions.c
+++ functions.c
@@ -69,7 +69,7 @@ int handle_spec(const char *format, int i, va_list args)
 			l += ops[j].f(args);
 			break;
 		}
-		if (j == 10)
+		if (j == 11)
 		{
 			write(1, "%", 1);
 			write(1, &format[i], 1);
